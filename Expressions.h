fe41de0ec498b300aa9b279f921be2ff9e97e656
Alexey Shishkov <sha43@yandex-team.ru>
2020-04-05 00:43:21 +0300

    Classified expressions by number of params

--- Expressions.h
+++ Expressions.h
@@ -7,102 +7,145 @@
 
 #include <string>
 #include <memory>
+#include <utility>
+
+class NoSuchExpressionType : std::exception {
+};
+
+enum OneArgumentExpressionType {
+    NOT_EXPRESSION
+};
+
+enum TwoArgumentsExpressionType {
+    OR_EXPRESSION,
+    AND_EXPRESSION
+};
 
 class Expression {
 public:
-    virtual bool evaluate() const = 0;
-    virtual bool evaluate(bool* vars) const = 0;
-    virtual std::string toString() const = 0;
+    virtual bool evaluate(bool *vars) const = 0;
 
-    virtual ~Expression() {}
+    [[nodiscard]] virtual std::string toString() const = 0;
 
+    virtual ~Expression() = default;
 };
 
 using ExpressionPtr = std::shared_ptr<Expression>;
-
-
-class NotExpression : public Expression {
-private:
-    ExpressionPtr mhs;
-public:
-    NotExpression(ExpressionPtr mhs);
-
-    bool evaluate() const override;
-    bool evaluate(bool* vars) const override;
-
-
-    std::string toString() const override;
-
-    ~NotExpression();
-};
-
-class AndExpression : public Expression {
-private:
-    ExpressionPtr lhs;
-    ExpressionPtr rhs;
-public:
-    AndExpression(ExpressionPtr lhs, ExpressionPtr rhs);
-
-    bool evaluate() const override;
-    bool evaluate(bool* vars) const override;
-
-    std::string toString() const override;
-
-    ~AndExpression();
-};
-
-
-class OrExpression : public Expression {
-private:
-    ExpressionPtr lhs;
-    ExpressionPtr rhs;
-public:
-    OrExpression(ExpressionPtr lhs, ExpressionPtr rhs);
-
-    bool evaluate() const override;
-    bool evaluate(bool* vars) const override;
-
-    std::string toString() const override;
-
-    ~OrExpression();
-};
 
 class ConstantExpression : public Expression {
 private:
     bool value;
 public:
-    ConstantExpression(int value);
+    explicit ConstantExpression(int value) : value(value) {};
 
-    ConstantExpression(bool value);
+    explicit ConstantExpression(bool value) : value(value) {};
 
-    bool evaluate() const override;
-    bool evaluate(bool* vars) const override;
+    bool evaluate(bool *vars) const override {
+        return value;
+    }
 
-    std::string toString() const override;
+    [[nodiscard]] std::string toString() const override {
+        return std::to_string(value);
+    }
 
-    ~ConstantExpression();
+    ~ConstantExpression() override = default;
 };
 
 class VariableExpression : public Expression {
 private:
     size_t myIndex;
 public:
-    VariableExpression(size_t myIndex);
+    explicit VariableExpression(size_t myIndex) : myIndex(myIndex) {};
 
-    bool evaluate() const override;
-    bool evaluate(bool* vars) const override;
+    bool evaluate(bool *vars) const override {
+        return vars[myIndex];
+    }
 
-    std::string toString() const override;
+    [[nodiscard]] std::string toString() const override {
+        return "x_" + std::to_string(myIndex);
+    }
 
-    ~VariableExpression();
+    ~VariableExpression() override = default;
 };
 
-ExpressionPtr Constant(bool x);
+class OneArgumentExpression : public Expression {
+private:
+    OneArgumentExpressionType type;
+    ExpressionPtr argument;
+public:
+    OneArgumentExpression(OneArgumentExpressionType type, ExpressionPtr argument) : type(type),
+                                                                                    argument(std::move(argument)) {}
 
-ExpressionPtr Or(ExpressionPtr x, ExpressionPtr y);
+    bool evaluate(bool *vars) const override {
+        if (type == NOT_EXPRESSION) {
+            return !(argument->evaluate(vars));
+        } else {
+            throw NoSuchExpressionType();
+        }
+    }
 
-ExpressionPtr And(ExpressionPtr x, ExpressionPtr y);
+    [[nodiscard]] std::string toString() const override {
+        return "!(" + argument->toString() + ")";
+    }
 
-ExpressionPtr Not(ExpressionPtr x);
+    ~OneArgumentExpression() override = default;
+};
+
+class TwoArgumentsExpression : public Expression {
+private:
+    TwoArgumentsExpressionType type;
+    ExpressionPtr first_argument;
+    ExpressionPtr second_argument;
+public:
+    TwoArgumentsExpression(TwoArgumentsExpressionType type, ExpressionPtr fist_argument,
+                           ExpressionPtr second_argument) :
+            type(type), first_argument(std::move(fist_argument)), second_argument(std::move(second_argument)) {}
+
+    bool evaluate(bool *vars) const override {
+        if (type == OR_EXPRESSION) {
+            return first_argument->evaluate(vars) || second_argument->evaluate(vars);
+        } else if (type == AND_EXPRESSION) {
+            return first_argument->evaluate(vars) && second_argument->evaluate(vars);
+        } else {
+            throw (NoSuchExpressionType());
+        }
+    }
+
+    [[nodiscard]] std::string toString() const override {
+        if (type == OR_EXPRESSION) {
+            return "(" + first_argument->toString() + ")|(" + second_argument->toString() + ")";
+        } else if (type == AND_EXPRESSION) {
+            return "(" + first_argument->toString() + ")&(" + second_argument->toString() + ")";
+        } else {
+            throw (NoSuchExpressionType());
+        }
+    }
+
+    ~TwoArgumentsExpression() override = default;
+};
+
+// --------------------- Functions --------------------- //
+ExpressionPtr Constant(bool x) {
+    return std::make_shared<ConstantExpression>(ConstantExpression(x));
+}
+
+ExpressionPtr Variable(size_t index) {
+    return std::make_shared<VariableExpression>(VariableExpression(index));
+}
+
+ExpressionPtr Not(ExpressionPtr argument) {
+    return std::make_shared<OneArgumentExpression>(OneArgumentExpression(NOT_EXPRESSION, std::move(argument)));
+}
+
+ExpressionPtr Or(ExpressionPtr first_argument, ExpressionPtr second_argument) {
+    return std::make_shared<TwoArgumentsExpression>(
+            TwoArgumentsExpression(OR_EXPRESSION, std::move(first_argument), std::move(second_argument)));
+}
+
+ExpressionPtr And(ExpressionPtr first_argument, ExpressionPtr second_argument) {
+    return std::make_shared<TwoArgumentsExpression>(
+            TwoArgumentsExpression(AND_EXPRESSION, std::move(first_argument), std::move(second_argument)));
+}
+
 
 #endif //FIXEDTOPOLOGY_EXPRESSIONS_H
