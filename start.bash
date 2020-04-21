c13d20539403aa73cac6908e98af4d9e79f6da2c
Alexey Shishkov <sha43@yandex-team.ru>
2020-04-21 17:56:55 +0300

    Updated start.bash

--- start.bash
+++ start.bash
@@ -1,5 +1,5 @@
 for ((i=1; i <= 9; i++))
 do
-gnome-terminal -e "bash -c \"cd ~/CLionProjects/FixedTopology && ./bin/fixedTopology\""
+gnome-terminal -e "bash -c \"./bin/fixedTopology\""
 sleep 2
 done
